17fc8062ebc3848347956d34bf3a926c57c7075b
Ben Sampica <benjamin.sampica@gmail.com>
2025-06-07 20:03:50 -0500

    Update master-client-machine-setup.ps1

--- clients/master-client-machine-setup.ps1
+++ clients/master-client-machine-setup.ps1
@@ -9,16 +9,5 @@ net localgroup Administrators $adminUser /add
 # Hides the account so users cannot see it.
 New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $adminUser -Value 0 -PropertyType DWord
 
-# Add schedule task to run a custom script (lancache sync, etc.) on startup.
-$taskName = "LANStartupSync"
-$serverBootScriptLocation = "\\192.168.1.100\sync\on_boot.ps1"
-$trigger = New-ScheduledTaskTrigger -AtStartup
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File $serverBootScriptLocation"
-$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest
-
-Register-ScheduledTask -TaskName $taskName -Trigger $trigger -Action $action -Principal $principal -Force
-
-Write-Host "Scheduled task '$taskName' registered."
-
 # Prepares the client for imaging by removing machine specific stuff.
 C:\Windows\System32\Sysprep\sysprep.exe /oobe /generalize /shutdown
