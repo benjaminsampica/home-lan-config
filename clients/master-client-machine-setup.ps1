5aa2ab9b83297f91cfcdb66c9bbd53c9b5e27945
benjaminsampica <benjamin.sampica@gmail.com>
2025-06-07 19:16:21 -0500

    make vars consistent

--- clients/master-client-machine-setup.ps1
+++ clients/master-client-machine-setup.ps1
@@ -1,13 +1,13 @@
 # Run this on the first client PC to setup. Change the admin name & password to whatever you want.
-$adminName = "LANAdmin"
-$password = "SuperSecret123"
+$adminUser = "LANAdmin"
+$adminPass = "SuperSecret123"
 
 # Adds a user account as an admin.
-net user $adminName $password /add
-net localgroup Administrators $adminName /add
+net user $adminUser $adminPass /add
+net localgroup Administrators $adminUser /add
 
 # Hides the account so users cannot see it.
-New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $adminName -Value 0 -PropertyType DWord
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $adminUser -Value 0 -PropertyType DWord
 
 # Prepares the client for imaging by removing machine specific stuff.
 C:\Windows\System32\Sysprep\sysprep.exe /oobe /generalize /shutdown
